f42a81f2f2abc9df9020880e8ab36ff5801da8b8
Awilum <awilum@msn.com>
2020-10-14 18:22:38 +0300

    update helpers

--- src/helpers.php
+++ src/helpers.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use Atomastic\Filesystem\Filesystem;
 
 if (! function_exists('filesystem')) {
+    /**
+     * Create a Filesystem instance.
+     */
     function filesystem(): Filesystem
     {
         return new Filesystem();
